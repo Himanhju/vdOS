ba1264a7a01d67bd4e2d0125b8601ab4e8c125bb
Himanhju <152820359+Himanhju@users.noreply.github.com>
2025-04-16 21:41:26 -0400

    Update and rename Makefile to build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+nasm -f bin bootloader.asm -o asm.bin;
+clang -target i386-pc-none-elf -ffreestanding -fno-builtin -fomit-frame-pointer -fno-stack-protector -fno-pic -fno-pie -m16 -mno-red-zone -mno-stackrealign -nostdlib -Wall -Wextra -c kernel.c -o o.o;
+ld.lld -Ttext 0x7E00 --oformat binary o.o -o c.bin;
+truncate --size=5120 c.bin;
+dd if=/dev/zero of=vdOS.img bs=512 count=20480;
+dd if=asm.bin of=vdOS.img bs=512 count=1 conv=notrunc;
+dd if=c.bin of=vdOS.img bs=512 seek=1 conv=notrunc;
+
+#qemu-system-x86_64 -drive file=vdOS.img,format=raw,media=disk to run
